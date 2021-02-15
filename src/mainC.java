0bd4083d39ee32ca833ebfeac1a2e9dc44c3156d
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-02-15 16:31:07 +0100

    d1

---
+++ src/mainC.java
@@ -0,0 +1,12 @@
+public class mainC {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void future(){
+        System.out.println("My name is Phuong, and my horoscope is Scorpio.");
+    }
+
+}
+
