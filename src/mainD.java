2d7d0168bf34a77e91387abfb8d7641ea66dbfa9
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-02-15 16:44:03 +0100

    d2, hejjo

--- src/mainD.java
+++ src/mainD.java
@@ -7,6 +7,7 @@ public class mainD {
     public void future(String name){
         System.out.println("My name is Phuong, and my horoscope is Scorpio.");
         System.out.println(name);
+        System.out.println("hejjo");
     }
 
 }
