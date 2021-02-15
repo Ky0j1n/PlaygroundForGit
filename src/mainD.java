20c148e9e5f03818617ff26aba1e77ee3a83472e
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-02-15 17:18:26 +0100

    d5

--- src/mainD.java
+++ src/mainD.java
@@ -4,10 +4,11 @@ public class mainD {
 
     }
 
-    public void future(String name){
+    public void future(String name, Integer penisSize){
         System.out.println("My name is Phuong, and my horoscope is Dog.");
         System.out.println(name);
         System.out.println("hejjo");
+        System.out.println(name + "," + "penis size:" + penisSize);
     }
 
 }
