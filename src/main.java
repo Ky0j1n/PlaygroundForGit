b5644af13f52865208ad21aab7b5fda606f53e06
Mate Marko <markomate90@gmail.com>
2021-02-15 17:13:52 +0100

    a5

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
     }
 
-    public void greet(Integer piratePenisSize){
+    public void greet(Integer piratePenisSize, String tuff){
         System.out.println("Your name (Mate)" + piratePenisSize);
         System.out.println("Okairi Longo Penisu Pirato-sama!");
     }
